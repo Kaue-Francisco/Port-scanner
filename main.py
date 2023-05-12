e28c3ce55dad2d5b932b3460cb1d6230abdf13a3
Kaue-Francisco <kauesantos.francisco@gmail.com>
2023-05-12 15:42:18 -0300

    arrumando identação

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ ports = [20,21,22,25,53,80,123,179,443,500,3389]
 
 #scan das portas e seu resultado
 for i in ports:
-   client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-   client.settimeout(0.2)
-   connection = client.connect_ex((ip,i))
-   if connection == 0:
-       print(i, '>>> OPEN')
-   else:
-       pass
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.settimeout(0.2)
+    connection = client.connect_ex((ip,i))
+    if connection == 0:
+        print(i, '>>> OPEN')
+    else:
+        pass
